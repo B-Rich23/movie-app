5f6eb64ffc5a89d668b1ce710e7031c8fc3105ca
Brandan Richardson <brandanr@gmail.com>
2018-07-07 23:13:14 -0700

    created Search component

--- src/Search.js
+++ src/Search.js
@@ -2,24 +2,18 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import MovieList from './MovieList';
 import MovieDetails from './MovieDetails';
-import Search from './Search';
 
 
-const App = () => (
+const Search = () => (
   <Router>
-    <div className="App">
-      <header className="App-header">
+    <div className="Search">
+      {/* <header className="App-header">
         <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
         </Link>
-        <Search />
-      </header>
+      </header> */}
       <Switch>
-        <Route exact path="/" component={MovieList} />
         <Route path="/:id" component={MovieDetails} />
       </Switch>
     </div>
@@ -27,4 +21,4 @@ const App = () => (
 );
 
 
-export default App;
+export default Search;
